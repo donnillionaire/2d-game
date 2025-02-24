c000d2270f3bb3684edf7f03963b5f9dc2f29bf1
donnillionaire <donaldodhiambo09@gmail.com>
2025-02-24 18:10:47 +0300

    feat: created score function

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int playerScore;
+    public Text scoreText;
+
+
+    [ContextMenu("Increase Score")]
+    public void addScore (){
+        playerScore = playerScore + 1;
+        scoreText.text = playerScore.ToString();
+    }
+   
+}
