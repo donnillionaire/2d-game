45e46b3e767e2c2de55496793e70b7a0ca82cf56
donnillionaire <donaldodhiambo09@gmail.com>
2025-02-24 13:53:31 +0300

    feat: initiate pipe spawner

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -1,10 +1,7 @@
 using UnityEngine;
 
-public class PIpeMoveScript : MonoBehaviour
+public class PipeSpawnScript : MonoBehaviour
 {
-
-    public float moveSpeed = 5;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +11,6 @@ public class PIpeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left*moveSpeed) * Time.deltaTime;
         
     }
 }
