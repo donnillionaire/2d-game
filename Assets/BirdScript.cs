88bd8493c9b53df2fa86f532140a98699d7c1cfc
donnillionaire <donaldodhiambo09@gmail.com>
2025-02-24 13:19:49 +0300

    feat: added reference to rigid body 2d with the script, now the script can communicate directly to rigid body feature

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidbody; // reference to rigid body 2d
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
