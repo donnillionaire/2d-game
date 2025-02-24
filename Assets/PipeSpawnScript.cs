7a4b099f8e1325f57dc72ccdae8222a236d2192a
donnillionaire <donaldodhiambo09@gmail.com>
2025-02-24 14:04:41 +0300

    feat: spawn pipes feature is added

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class PipeSpawnScript : MonoBehaviour
 {
+    public GameObject pipe;
+
+    public float spawnRate =2;
+    private float timer = 0;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spawnPipe();//spawn the first pipes
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        if (timer < spawnRate){
+            timer = timer + Time.deltaTime;
+        }else{
+
+            spawnPipe();
+            timer = 0;
+
+        }
+        
         
     }
+    void spawnPipe(){
+        Instantiate(pipe, transform.position, transform.rotation); //spawn new pipes
+
+    }
+
+
+
+
 }
